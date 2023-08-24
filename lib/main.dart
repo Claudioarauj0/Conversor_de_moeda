01c2e3b9bd39296d5d1de65ed4f1ce99b470ba20
Claudioarauj0 <claudinhosas@gmail.com>
2023-08-24 11:11:45 -0300

    alterações no projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
-void main (){
-  runApp( const MaterialApp(
- home: Home(),
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
   ));
 }
 
@@ -10,14 +11,47 @@ class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
-  State<Home> createState() => _homeState();
+  State<Home> createState() => HomeState();
 }
 
-class _homeState extends State<Home> {
+class HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
-    );
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Conversor", style: TextStyle(color: Colors.black)),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+      body: const SingleChildScrollView(
+child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Icon(
+              Icons.monetization_on_outlined,
+              size: 120,
+              color: Colors.black,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                  labelText: "Reais",
+                  labelStyle: TextStyle(color: Colors.black)),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                  labelText: "Dolar",
+                  labelStyle: TextStyle(color: Colors.black)),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                  labelText: "Euro",
+                  labelStyle: TextStyle(color: Colors.black)),
+            )
+          ],
+        ),
+      ),
+      )
+    );    
   }
 }
